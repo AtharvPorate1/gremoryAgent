9d459793819b8d93b8585e2139d7fb7873ad96ca
AtharvPorate1 <www.atharvporate254@gmail.com>
2025-05-25 19:27:10 +0530

    file name issue solved

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 import serverless from 'serverless-http';
-import app from '../src/server'; // Adjust path based on your structure
+import app from '../src/server.js'; // Adjust path based on your structure
 
 export const handler = serverless(app);
